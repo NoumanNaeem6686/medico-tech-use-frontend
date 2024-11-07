9c8fcdd04caccdbda2b387f5939b4afa5c572eaf
Muhammad Nouman <54278279+NoumanNaeem6686@users.noreply.github.com>
2024-11-07 11:36:55 +0500

    Update _document.js

--- src/app/_document.js
+++ src/app/_document.js
@@ -8,7 +8,7 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head>
-        {/* Add the Font Awesome CDN link here */}
+        {/* Add the Font Awesome  CDN link here */}
         <link
           rel="stylesheet"
           href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css"
