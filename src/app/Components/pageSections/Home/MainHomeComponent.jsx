8ffbcea1b946220dbee04d3d1c411d08667dd3b0
Saif Ur Rehman <saifarshad3344@gmail.com>
2024-11-18 13:38:41 +0500

    feat : added a CTA

--- src/app/Components/pageSections/Home/MainHomeComponent.jsx
+++ src/app/Components/pageSections/Home/MainHomeComponent.jsx
@@ -9,6 +9,7 @@ import AboutUs from "./About-Us";
 import OurServices from './Our-Services'
 import Services from "./Services";
 import GrowthNumbers from "./Growth";
+import Link from "next/link";
 const MainHomeComponent = () => {
   return (
     <div>
@@ -16,6 +17,47 @@ const MainHomeComponent = () => {
       <AboutUs />
       <Services />
       <GrowthNumbers />
+      <section className="py-16">
+        <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
+          <div
+            class="lg:py-14 lg:px-20 p-10 rounded-2xl flex items-center justify-between flex-col lg:flex-row"
+            style={{ background: "linear-gradient(to right, #050A30, #107ACC);" }}
+          >
+            <div className="block text-center mb-5 lg:text-left lg:mb-0 sm:max-w-[600px]">
+              <h2
+                className="font-manrope text-4xl text-white font-semibold mb-5 lg:mb-2"
+              >
+                Partner with Us for Healthcare Excellence
+              </h2>
+              <p className="text-xl text-indigo-100">
+                Let’s revolutionize healthcare together. Reach out for customized solutions or to explore how we can empower your practice.
+              </p>
+
+            </div>
+            <Link
+              href="#"
+              className="flex items-center gap-2 bg-white rounded-full shadow-sm text-lg text-indigo-600 font-semibold py-4 px-8 transition-all duration-500"
+            >Get In Touch
+              <svg
+                width="19"
+                height="14"
+                viewBox="0 0 19 14"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  d="M1.75 7L16.4167 7M11.8333 12.5L16.6852 7.64818C16.9907 7.34263 17.1435 7.18985 17.1435 7C17.1435 6.81015 16.9907 6.65737 16.6852 6.35182L11.8333 1.5"
+                  stroke="#4F46E5"
+                  stroke-width="2.4"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                />
+              </svg>
+            </Link>
+          </div>
+        </div>
+      </section>
+
       {/* <About /> */}
       <HelpLine />
       <Certification />
